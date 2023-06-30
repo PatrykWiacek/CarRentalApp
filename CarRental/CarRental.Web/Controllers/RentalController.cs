f4a1b0ed1cd8ab9a3b51d07b0edd1a5aae1ac1b5
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-06-30 23:35:48 +0200

    pare zmian do prfzekazzywania id uzytkownika

--- CarRental/CarRental.Web/Controllers/RentalController.cs
+++ CarRental/CarRental.Web/Controllers/RentalController.cs
@@ -1,6 +1,7 @@
 ﻿using CarRental.Logic.Models;
 using CarRental.Logic.Services.IServices;
 using CarRental.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarRental.Web.Controllers;
