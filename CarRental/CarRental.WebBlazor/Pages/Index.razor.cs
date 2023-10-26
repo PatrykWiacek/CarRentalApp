709cab1f741867c202d11fabf0da0bf8d23ae486
Patryk Wiacek <117929073+PatrykWiacek@users.noreply.github.com>
2023-10-26 13:13:09 +0200

    Blazor project

---
+++ CarRental/CarRental.WebBlazor/Pages/Index.razor.cs
@@ -0,0 +1,10 @@
+﻿using CarRental.Logic.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace CarRental.WebBlazor.Pages;
+
+public partial class Index
+{
+    public SearchFieldsModel SearchDto { get; set; } = new SearchFieldsModel();
+    public IEnumerable<CarViewModel> Cars { get; set; } = new List<CarViewModel>();
+}
